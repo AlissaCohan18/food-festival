f0056721b88dee6a274723d1fcb0568157c51998
Alissa Cohan <alissacohan18@gmail.com>
2023-03-04 15:00:26 -0500

    added manifest.json

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 const webpack = require("webpack");
 const BundleAnalyzerPlugin =
   require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 
 //config file isn't necessary, but want to use it to be specific w/ how webpack will function
 
@@ -12,7 +13,7 @@ const config = {
     app: "./assets/js/script.js",
     events: "./assets/js/events.js",
     schedule: "./assets/js/schedule.js",
-    tickets: "./assets/js/tickets.js"
+    tickets: "./assets/js/tickets.js",
   },
   //webpack takes the entry point, bundles code, & outputs bundled code to folder specified (dist)
   output: {
@@ -21,35 +22,35 @@ const config = {
   },
   devServer: {
     static: {
-      directory: __dirname
-    }
+      directory: __dirname,
+    },
   },
   module: {
     rules: [
       {
-        //use the test property to find a regex (any image file with .jpg extension)
+        //use the test property to find a regex (any image file with jpg, png or gif extension)
         //targeting carousel images to optimize
-        test: /\.jpg$/i,
+        test: /\.(png|jpe?g|gif)$/i,
         //add the use property where the actual loader is implemented
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
               esModule: false,
               name(file) {
-                return '[path][name].[ext]';
+                return "[path][name].[ext]";
               },
-              publicPath: function(url) {
-                return url.replace('../', '/assets/');
-              }
-            }
+              publicPath: function (url) {
+                return url.replace("../", "/assets/");
+              },
+            },
           },
           {
-            loader: 'image-webpack-loader'
-          }
-        ]
-      }
-    ]
+            loader: "image-webpack-loader",
+          },
+        ],
+      },
+    ],
   },
   //this let's webpack know to use plugins (which in this case is the jQuery package)
   plugins: [
@@ -58,10 +59,27 @@ const config = {
       jQuery: "jquery",
     }),
     new BundleAnalyzerPlugin({
-        //with value set to "static" the report outputs to an HTML file in the dist folder; could also
-        //set to "disable" to temp stop the reporting & automatic opening of this report in browser
+      //with value set to "static" the report outputs to an HTML file in the dist folder; could also
+      //set to "disable" to temp stop the reporting & automatic opening of this report in browser
       analyzerMode: "static",
-      
+    }),
+    new WebpackPwaManifest({
+      name: "Food Event",
+      short_name: "Foodies",
+      description: "An app that allows you to view upcoming food events.",
+      //specify homepage for the PWA relative to the location of the manifest file
+      start_url: "../index.html",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      fingerprints: false,
+      inject: false,
+      icons: [
+        {
+          src: path.resolve("assets/img/icons/icon-512x512.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets", "icons"),
+        },
+      ],
     }),
   ],
   //provide mode we want webpack to run (default for webpack is to run in "production" mode)
